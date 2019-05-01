bc5e6e8b2b3569291dd995f7d5bfebcfcee9e954
WORLAND <bw0894325@otc.edu>
2019-05-01 16:04:05 -0500

    essage

---
+++ script.js
@@ -0,0 +1,95 @@
+
+/// https://api.themoviedb.org/3/movie/550?api_key=1ffdd6733bf161b583036b78a699b120
+
+///example api
+
+/* $("#2").click(function()
+{
+    url = "https://launchlibrary.net/1.4/launch/next/52"
+    http.open("GET", url);
+    http.send();
+    http.onreadystatechange=(e)=>
+    {
+
+        if (http.readyState == 4 && http.status == 200)//this is the final important readyState)
+        {
+
+    
+        }
+    }
+
+});
+
+$("#3").click(function()
+{
+    url = "https://launchlibrary.net/1.4/launch/next/49"
+    http.open("GET", url);
+    http.send();
+    http.onreadystatechange=(e)=>
+    {
+
+        if (http.readyState == 4 && http.status == 200)//this is the final important readyState)
+        {
+                jsonObj = JSON.parse(http.responseText);
+
+                $("#rocket1").text(jsonObj.launches[21].name + ' ' + jsonObj.launches[21].windowstart);
+                $("#rocket2").text(jsonObj.launches[42].name + ' ' + jsonObj.launches[42].windowstart);
+                $("#rocket3").text(jsonObj.launches[43].name + ' ' + jsonObj.launches[43].windowstart);
+                $("#rocket4").text(jsonObj.launches[47].name + ' ' + jsonObj.launches[47].windowstart);
+                $("#rocket5").text(jsonObj.launches[48].name + ' ' + jsonObj.launches[48].windowstart);
+
+        }
+    }
+
+});
+
+$("#4").click(function() {
+    url = "https://launchlibrary.net/1.4/launch/next/5"
+    http.open("GET", url);
+    http.send();
+    http.onreadystatechange=(e)=>
+    {
+
+        if (http.readyState == 4 && http.status == 200)//this is the final important readyState)
+        {
+                jsonObj = JSON.parse(http.responseText);
+
+                $("#rocket1").text(jsonObj.launches[0].name + ' ' + jsonObj.launches[0].windowstart);
+                $("#rocket2").text(jsonObj.launches[1].name + ' ' + jsonObj.launches[1].windowstart);
+                $("#rocket3").text(jsonObj.launches[2].name + ' ' + jsonObj.launches[2].windowstart);
+                $("#rocket4").text(jsonObj.launches[3].name + ' ' + jsonObj.launches[3].windowstart);
+                $("#rocket5").text(jsonObj.launches[4].name + ' ' + jsonObj.launches[4].windowstart);
+        }
+    }
+}); */ 
+
+
+/* //Basic Syntax
+//$("selector").action()
+EXAMPLE JQUERY
+$(document).ready(function()
+{
+    $('h1').html('AHAHAHAHA');
+
+    $('p').click(function(){
+       alert('BUTTS');
+    })
+   
+    $('p').mouseenter(function(){
+        $(this).css('background-color', 'pink');
+    })
+});
+
+
+
+$('p').on ({
+    click : function(){
+       alert('BUTTS');
+    },
+   
+    mouseenter : function(){
+        $(this).css('background-color', 'pink');
+    
+    }
+    
+}); */
